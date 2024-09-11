28400366a089fb4b5561227afd395fa09f911dc5
ElderBike4 <estebanelder4@gmail.com>
2024-09-11 09:54:29 -0600

    Pruebas comentadas

--- src/web/scripts.js
+++ src/web/scripts.js
@@ -1,17 +1,17 @@
 function sumar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    document.getElementById('resultado').innerText = num1 + num2;
+    //var num1 = parseInt(document.getElementById('num1').value);
+    //var num2 = parseInt(document.getElementById('num2').value);
+    //document.getElementById('resultado').innerText = num1 + num2;
 }
 
 function restar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    document.getElementById('resultado').innerText = num1 - num2;
+    //var num1 = parseInt(document.getElementById('num1').value);
+    //var num2 = parseInt(document.getElementById('num2').value);
+    //document.getElementById('resultado').innerText = num1 - num2;
 }
 
 function multiplicar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    document.getElementById('resultado').innerText = num1 * num2;
+    //var num1 = parseInt(document.getElementById('num1').value);
+    //var num2 = parseInt(document.getElementById('num2').value);
+    //document.getElementById('resultado').innerText = num1 * num2;
 }
